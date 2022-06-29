cc90715f2a505257f50510fb3a18bea40ca5fbb4
Clement <clement_392@hotmail.com>
2022-06-29 01:56:25 -0700

    Update PyWebCrawl with depth search

--- PyWebCrawl.py
+++ PyWebCrawl.py
@@ -1,123 +1,138 @@
+# Import libraries
 import sys
-import requests
-import requests.exceptions
+from urllib.request import urljoin
 from bs4 import BeautifulSoup
-from collections import deque
-from time import sleep
-from urllib.parse import urlsplit
-from urllib.parse import urlparse
+import requests
+from urllib.request import urlparse
 
 
-class PyWebCrawl:
-    def __init__(self, start_url):
+# Set for storing urls with same domain
+links_intern = set()
 
-        # init name of crawler
-        self.name = "webapp_crawler"
+# usage message
+usage_msg = f"Usage: {sys.argv[0]} http[s]://<domain> <depth_to_crawl>\nDepth: 0-10"
 
-        # init counter from 0
-        self.counter = 0
+# Set for storing urls with different domain
+links_extern = set()
 
-        # init url from param
-        if not start_url.endswith("/"):  # check if ends with a slash or not, if not, then add one
-            start_url = start_url + "/"
-        if not start_url.startswith("https://") and not start_url.startswith("http://"):
-            start_url = "http://" + start_url
-        self.url = start_url
 
-        # a queue of urls to be crawled next
-        self.new_urls = deque([self.url])
+# Method for crawling a url at next level
+def level_crawler(input_url):
+	temp_urls = set()
+	current_url_domain = urlparse(input_url).netloc
 
-        # a set of urls that we have already processed
-        self.processed_urls = set()
+	# Creates beautiful soup object to extract html tags
+	beautiful_soup_object = BeautifulSoup(
+		requests.get(input_url).content, "lxml")
 
-        # a set of domains inside the target website
-        self.local_urls = set()
+	# Access all anchor tags from input
+	# url page and divide them into internal
+	# and external categories
+	for anchor in beautiful_soup_object.findAll("a"):
+		href = anchor.attrs.get("href")
+		if(href != "" or href != None):
+			href = urljoin(input_url, href)
+			href_parsed = urlparse(href)
+			href = href_parsed.scheme
+			href += "://"
+			href += href_parsed.netloc
+			href += href_parsed.path
+			final_parsed_href = urlparse(href)
+			is_valid = bool(final_parsed_href.scheme) and bool(
+				final_parsed_href.netloc)
+			if is_valid:
+				if 'http' not in href:
+					continue
+				if current_url_domain not in href and href not in links_extern:
+					#print("Extern - {}".format(href))
+					links_extern.add(href)
+				if current_url_domain in href and href not in links_intern:
+					# internal links only
+					print(href)
+					links_intern.add(href)
+					temp_urls.add(href)
+	return temp_urls
 
-        # a set of domains outside the target website
-        self.foreign_urls = set()
+def start_crawl(input_url, depth):
+	if(depth == 0):
+		print("Intern - {}".format(input_url))
 
-        # a set of broken urls
-        self.broken_urls = set()
+	elif(depth == 1):
+		level_crawler(input_url)
+	else:
+		# We have used a BFS approach
+		# considering the structure as
+		# a tree. It uses a queue based
+		# approach to traverse
+		# links upto a particular depth.
+		queue = []
+		queue.append(input_url)
+		for j in range(depth):
+			for count in range(len(queue)):
+				url = queue.pop(0)
+				urls = level_crawler(url)
+				for i in urls:
+					queue.append(i)
+	return list(links_intern)
+	
+def test_url(appended_input_url):
+	response = requests.get(appended_input_url)
+	if response.status_code == 200:
+	    return True
+	else:
+	    return False
 
-        # a set of visited url
-        self.visited_urls = set()
-        
-        # add list of blacklisted pages or words to avoid spidering them
-        self.lst_blacklist = ['..', 'mailto','cdn','token']
-
-    def crawl(self):
-        # process urls one by one until we exhaust the queue
-        while self.new_urls:
-            # move url from the queue to processed url set
-            url = self.new_urls.popleft()
-            self.processed_urls.add(url)
-            # print the current url
-            # print(f"Processing {self.url}")
-
-            try:
-                response = requests.get(url)
-            except (
-                requests.exceptions.MissingSchema,
-                requests.exceptions.ConnectionError,
-                requests.exceptions.InvalidURL,
-                requests.exceptions.InvalidSchema,
-            ):
-                # add broken urls to it’s own set, then continue
-                self.broken_urls.add(url)
-                continue
-            # extract base url to resolve relative links
-            parts = urlsplit(url)
-            base = "{0.netloc}".format(parts)
-            strip_base = base.replace("www.", "")
-            base_url = "{0.scheme}://{0.netloc}".format(parts)
-            path = url[: url.rfind("/") + 1] if "/" in parts.path else url
-            # check if ending with a /, if not, add a /
-
-            # create a beautiful soup for the html document
-            soup = BeautifulSoup(response.text, "lxml")
-
-            for link in soup.find_all("a"):
-                # bool contains blacklist
-                bool_contains_blacklist = False
-            
-                # extract link url from the anchor
-                anchor = link.attrs["href"] if "href" in link.attrs else ""
-                
-                for blacklisted in self.lst_blacklist:
-                    if blacklisted in anchor:
-                        bool_contains_blacklist = True
-                if bool_contains_blacklist:
-                    continue
-                if anchor.startswith("/"):
-                    local_link = base_url + anchor
-                    self.local_urls.add(local_link)
-                elif strip_base in anchor:
-                    self.local_urls.add(anchor)
-                elif not anchor.startswith("http"):
-                    local_link = path + anchor
-                    self.local_urls.add(local_link)
-                else:
-                    self.foreign_urls.add(anchor)
-                for i in self.local_urls:
-                    if not i in self.new_urls and not i in self.processed_urls:
-                        self.new_urls.append(i)
-                        print(i)
-        # for processed_url in self.processed_urls:
-        # print(processed_url)
-        return self.processed_urls
-
+def validate_inputs(input_url, depth):
+	# make sure that the number of parameters are correct
+	if len(sys.argv) != 3:
+		print(usage_msg)
+		sys.exit(1)
+	# make sure that depth that the user input is an integer
+	try:
+		depth = int(depth)
+		if depth > 10:
+			print(usage_msg)
+			sys.exit(1)
+	except ValueError as e:
+		print(usage_msg)
+		print('Depth must be an integer')
+		sys.exit(1)
+	# make sure that the url that is provided starts with http:// or https://
+	if not input_url.startswith('http://') or not input_url.startswith('https://'):
+		# means that input_url doesnt starts with http or https 
+		# we append http and https and check if it exist
+		# have a flag to check http or https
+		test_flag = 0
+		if test_url(f"http://{input_url}"):
+			test_flag += 1
+		if test_url(f"http://{input_url}"):
+			test_flag += 2
+		# now we check whats the value
+		# 0, means none works AKA invalid link provided
+		if test_flag == 0:
+			print(usage_msg)
+			print('Please provide a value url')
+		# 1, means that only http works
+		if test_flag == 1:
+			input_url = f"http://{input_url}"
+		# 2, means that only https works
+		if test_flag == 2:
+			input_url = f"https://{input_url}"
+		# 3, means both http and https works, we just choose http
+		if test_flag == 3:
+			input_url = f"http://{input_url}"
+		
+		
+	return input_url, depth
 
 def main():
-    # Ensure that the program is supplied a url to start crawling
-    if len(sys.argv) != 2:
-        print("Please provide a url to crawl")
-        sys.exit()
-    start_url = sys.argv[1]  # can use http://books.toscrape.com/ for testing
-    spider = PyWebCrawl(start_url)
-    res = spider.crawl()
-    print(res)
-    print(len(res))
-
-
+	input_url = sys.argv[1]
+	depth = sys.argv[2]
+	
+	input_url, depth = validate_inputs(input_url, depth)
+	
+	print(start_crawl(input_url, depth))
+	
 if __name__ == "__main__":
-    main()
+	main()
+
---
+++ PyWebCrawl_old.py
@@ -0,0 +1,123 @@
+import sys
+import requests
+import requests.exceptions
+from bs4 import BeautifulSoup
+from collections import deque
+from time import sleep
+from urllib.parse import urlsplit
+from urllib.parse import urlparse
+
+
+class PyWebCrawl:
+    def __init__(self, start_url):
+
+        # init name of crawler
+        self.name = "webapp_crawler"
+
+        # init counter from 0
+        self.counter = 0
+
+        # init url from param
+        if not start_url.endswith("/"):  # check if ends with a slash or not, if not, then add one
+            start_url = start_url + "/"
+        if not start_url.startswith("https://") and not start_url.startswith("http://"):
+            start_url = "http://" + start_url
+        self.url = start_url
+
+        # a queue of urls to be crawled next
+        self.new_urls = deque([self.url])
+
+        # a set of urls that we have already processed
+        self.processed_urls = set()
+
+        # a set of domains inside the target website
+        self.local_urls = set()
+
+        # a set of domains outside the target website
+        self.foreign_urls = set()
+
+        # a set of broken urls
+        self.broken_urls = set()
+
+        # a set of visited url
+        self.visited_urls = set()
+        
+        # add list of blacklisted pages or words to avoid spidering them
+        self.lst_blacklist = ['..', 'mailto','cdn','token']
+
+    def crawl(self):
+        # process urls one by one until we exhaust the queue
+        while self.new_urls:
+            # move url from the queue to processed url set
+            url = self.new_urls.popleft()
+            self.processed_urls.add(url)
+            # print the current url
+            # print(f"Processing {self.url}")
+
+            try:
+                response = requests.get(url)
+            except (
+                requests.exceptions.MissingSchema,
+                requests.exceptions.ConnectionError,
+                requests.exceptions.InvalidURL,
+                requests.exceptions.InvalidSchema,
+            ):
+                # add broken urls to it’s own set, then continue
+                self.broken_urls.add(url)
+                continue
+            # extract base url to resolve relative links
+            parts = urlsplit(url)
+            base = "{0.netloc}".format(parts)
+            strip_base = base.replace("www.", "")
+            base_url = "{0.scheme}://{0.netloc}".format(parts)
+            path = url[: url.rfind("/") + 1] if "/" in parts.path else url
+            # check if ending with a /, if not, add a /
+
+            # create a beautiful soup for the html document
+            soup = BeautifulSoup(response.text, "lxml")
+
+            for link in soup.find_all("a"):
+                # bool contains blacklist
+                bool_contains_blacklist = False
+            
+                # extract link url from the anchor
+                anchor = link.attrs["href"] if "href" in link.attrs else ""
+                
+                for blacklisted in self.lst_blacklist:
+                    if blacklisted in anchor:
+                        bool_contains_blacklist = True
+                if bool_contains_blacklist:
+                    continue
+                if anchor.startswith("/"):
+                    local_link = base_url + anchor
+                    self.local_urls.add(local_link)
+                elif strip_base in anchor:
+                    self.local_urls.add(anchor)
+                elif not anchor.startswith("http"):
+                    local_link = path + anchor
+                    self.local_urls.add(local_link)
+                else:
+                    self.foreign_urls.add(anchor)
+                for i in self.local_urls:
+                    if not i in self.new_urls and not i in self.processed_urls:
+                        self.new_urls.append(i)
+                        print(i)
+        # for processed_url in self.processed_urls:
+        # print(processed_url)
+        return self.processed_urls
+
+
+def main():
+    # Ensure that the program is supplied a url to start crawling
+    if len(sys.argv) != 2:
+        print("Please provide a url to crawl")
+        sys.exit()
+    start_url = sys.argv[1]  # can use http://books.toscrape.com/ for testing
+    spider = PyWebCrawl(start_url)
+    res = spider.crawl()
+    print(res)
+    print(len(res))
+
+
+if __name__ == "__main__":
+    main()
