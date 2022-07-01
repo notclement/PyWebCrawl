4939283021f90a1f8d157be4dcc892e08ce7dcc8
Clement <clement_392@hotmail.com>
2022-06-30 23:35:34 -0700

    Update PyWebCrawl with max depth

--- PyWebCrawl.py
+++ PyWebCrawl.py
@@ -5,73 +5,76 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.request import urlparse
 
-
-# Set for storing urls with same domain
-links_intern = set()
-
 # usage message
-usage_msg = f"Usage: {sys.argv[0]} http[s]://<domain> <depth_to_crawl>\nDepth: 0-10"
-
-# Set for storing urls with different domain
-links_extern = set()
+USAGE_MSG = f"Usage: {sys.argv[0]} http[s]://<domain> <depth_to_crawl>\nDepth: 0-10"
 
 
-# Method for crawling a url at next level
-def level_crawler(input_url):
-    temp_urls = set()
-    current_url_domain = urlparse(input_url).netloc
+class PyWebCrawl:
+    def __init__(self):
 
-    # Creates beautiful soup object to extract html tags
-    beautiful_soup_object = BeautifulSoup(requests.get(input_url).content, "lxml")
+        # Set for storing urls with same domain
+        self.links_intern = set()
 
-    # Access all anchor tags from input
-    # url page and divide them into internal
-    # and external categories
-    for anchor in beautiful_soup_object.findAll("a"):
-        href = anchor.attrs.get("href")
-        if href != "" or href != None:
-            href = urljoin(input_url, href)
-            href_parsed = urlparse(href)
-            href = href_parsed.scheme
-            href += "://"
-            href += href_parsed.netloc
-            href += href_parsed.path
-            final_parsed_href = urlparse(href)
-            is_valid = bool(final_parsed_href.scheme) and bool(final_parsed_href.netloc)
-            if is_valid:
-                if "http" not in href:
-                    continue
-                if current_url_domain not in href and href not in links_extern:
-                    # print("Extern - {}".format(href))
-                    links_extern.add(href)
-                if current_url_domain in href and href not in links_intern:
-                    # internal links only
-                    print(href)
-                    links_intern.add(href)
-                    temp_urls.add(href)
-    return temp_urls
+        # Set for storing urls with different domain
+        self.links_extern = set()
 
+    # Method for crawling a url at next level
+    def level_crawler(self, input_url):
+        temp_urls = set()
+        current_url_domain = urlparse(input_url).netloc
 
-def start_crawl(input_url, depth):
-    if depth == 0:
-        print("Intern - {}".format(input_url))
-    elif depth == 1:
-        level_crawler(input_url)
-    else:
-        # We have used a BFS approach
-        # considering the structure as
-        # a tree. It uses a queue based
-        # approach to traverse
-        # links upto a particular depth.
-        queue = []
-        queue.append(input_url)
-        for j in range(depth):
-            for count in range(len(queue)):
-                url = queue.pop(0)
-                urls = level_crawler(url)
-                for i in urls:
-                    queue.append(i)
-    return list(links_intern)
+        # Creates beautiful soup object to extract html tags
+        beautiful_soup_object = BeautifulSoup(
+            requests.get(input_url).content, "lxml")
+
+        # Access all anchor tags from input
+        # url page and divide them into internal
+        # and external categories
+        for anchor in beautiful_soup_object.findAll("a"):
+            href = anchor.attrs.get("href")
+            if href != "" or href != None:
+                href = urljoin(input_url, href)
+                href_parsed = urlparse(href)
+                href = href_parsed.scheme
+                href += "://"
+                href += href_parsed.netloc
+                href += href_parsed.path
+                final_parsed_href = urlparse(href)
+                is_valid = bool(final_parsed_href.scheme) and bool(
+                    final_parsed_href.netloc)
+                if is_valid:
+                    if "http" not in href:
+                        continue
+                    if current_url_domain not in href and href not in self.links_extern:
+                        # print("Extern - {}".format(href))
+                        self.links_extern.add(href)
+                    if current_url_domain in href and href not in self.links_intern:
+                        # internal links only
+                        print(href)
+                        self.links_intern.add(href)
+                        temp_urls.add(href)
+        return temp_urls
+
+    def crawl(self, input_url, depth):
+        if depth == 0:
+            print("Intern - {}".format(input_url))
+        elif depth == 1:
+            self.level_crawler(input_url)
+        else:
+            # We have used a BFS approach
+            # considering the structure as
+            # a tree. It uses a queue based
+            # approach to traverse
+            # links upto a particular depth.
+            queue = []
+            queue.append(input_url)
+            for j in range(depth):
+                for count in range(len(queue)):
+                    url = queue.pop(0)
+                    urls = self.level_crawler(url)
+                    for i in urls:
+                        queue.append(i)
+        return list(self.links_intern)
 
 
 def test_url(appended_input_url):
@@ -83,22 +86,26 @@ def test_url(appended_input_url):
             return False
     except:
         return False
-    
 
 
-def validate_inputs(input_url, depth):
+def validate_inputs():
     # make sure that the number of parameters are correct
     if len(sys.argv) != 3:
-        print(usage_msg)
+        print(USAGE_MSG)
         sys.exit(1)
+
+    # Already ensured that there are 2 parameters provided
+    input_url = sys.argv[1]
+    depth = sys.argv[2]
+
     # make sure that depth that the user input is an integer
     try:
         depth = int(depth)
         if depth > 10:
-            print(usage_msg)
+            print(USAGE_MSG)
             sys.exit(1)
     except ValueError as e:
-        print(usage_msg)
+        print(USAGE_MSG)
         print("Depth must be an integer")
         sys.exit(1)
     # make sure that the url that is provided starts with http:// or https://
@@ -114,7 +121,7 @@ def validate_inputs(input_url, depth):
         # now we check whats the value
         # 0, means none works AKA invalid link provided
         if test_flag == 0:
-            print(usage_msg)
+            print(USAGE_MSG)
             print("Please provide a value url")
             sys.exit(1)
         # 1, means that only http works
@@ -130,14 +137,15 @@ def validate_inputs(input_url, depth):
 
 
 def main():
-    input_url = sys.argv[1]
-    depth = sys.argv[2]
 
-    input_url, depth = validate_inputs(input_url, depth)
+    input_url, depth = validate_inputs()
 
-    print(start_crawl(input_url, depth))
+    spider = PyWebCrawl()
+
+    res = spider.crawl(input_url, depth)
+
+    print(res)
 
 
 if __name__ == "__main__":
     main()
-
